a864ee83b8782aa9692f83661b88a06f353d5958
Slimane Deb <cs_deb@esi.dz>
2018-10-19 11:07:29 +0100

    Using Test

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
